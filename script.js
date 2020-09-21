a0ffba071cab9a18011f557e40488bf0961ba6c9
Kaylee <99057337@mydavinci.nl>
2020-09-21 16:30:33 +0200

    Index.html en js bestand toegevoegd

---
+++ script.js
@@ -0,0 +1,20 @@
+var cheeseColor = prompt("Is jouw kaas geel?")
+
+if (cheeseColor == "ja") {
+	var cheeseholes = prompt("Zitten er gaten in?")
+}	
+else if(cheeseColor == "nee") {
+	var cheeseBlue = prompt("Heeft de kaas blauwe schimmels?")
+}
+if (cheeseholes == "ja") {
+	var cheeseExpensive = prompt("Is de kaas belachelijk duur?")
+}
+else if (cheeseholes == "nee") {
+	var cheeshard = prompt("Is de kaas hard als steen?")
+}
+if (cheeseExpensive == "ja") {
+	alert("Uw kaas is een Emmenthaler");
+}
+else if (cheeseExpensive == "nee") {
+	alert("Uw kaas is een Leerdammer");
+}
